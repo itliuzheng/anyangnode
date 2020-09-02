a3e7d62792ef8d30ec6d19194c79e09a9da5960b
刘正 <itliuzheng@sina.com>
2020-09-02 14:20:39 +0800

    mongodb

--- models/mongoose.js
+++ models/mongoose.js
@@ -1,19 +1,25 @@
-var mongoose = require('mongoose')
+var mongoose = require('mongoose');
+var fs = require('fs');
 
-mongoose.connect('mongodb://localhost/user',
-    {useNewUrlParser: true, useUnifiedTopology: true });
+fs.readFile('./mongodb.json','utf-8',(err,data)=>{
+   if(err){
+       console.log('读取不到数据库文件：'+ err);
+       return false
+   }else{
+       let Data = JSON.parse(data);
+       mongoose.connect(Data.address, {useNewUrlParser: true, useUnifiedTopology: true });
+       mongoose.connection.on('connected',function(){
+           console.log('数据库已连接')
+       });
+       mongoose.connection.on('error',function(err){
+           console.log('数据库连接异常：'+ err);
+       });
+       mongoose.connection.on('disconnected',function(){
+           console.log('数据库断开');
+       })
+   }
+});
 
-mongoose.connection.on('connected',function(){
-    console.log('数据库已连接')
-})
-
-mongoose.connection.on('error',function(err){
-    console.log('数据库连接异常：'+ err);
-})
-
-mongoose.connection.on('disconnected',function(){
-    console.log('数据库断开');
-})
 
 
 
